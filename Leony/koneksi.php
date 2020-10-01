8abb33d769d46f22bb1307906d7c12ec535975f1
saberwell04 <61140562+saberwell04@users.noreply.github.com>
2020-10-01 10:26:24 +0700

    Ngapus dlu

--- Leony/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $server     = "localhost";
-    $username   = "root";
-    $password   = "";
-    $database    = "db_reg";
-
-    
-    $koneksi = mysqli_connect($server, $username, $password, $database) or die("Koneksi ke database gagal");
-    ?>
